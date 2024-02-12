1974a122e83940d694ff900fa5a85b6d64ada9eb
Cbr97nyb <141534948+Cbr97nyb@users.noreply.github.com>
2024-02-12 00:33:42 -0500

    Create AuthController.cs
    Se crea API de inicio de sesión

---
+++ Back_Vinculacion_Fema/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using Back_Vinculacion_Fema.Models.DbModels;
+using Back_Vinculacion_Fema.Models.RequestModels;
+using Back_Vinculacion_Fema.Models.Utilidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Back_Vinculacion_Fema.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly vinculacionfemaContext _contexto;
+
+        public AuthController(vinculacionfemaContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Authenticate(UserLoginRequest credentials)
+        {
+            var encryptedPassword = credentials.Password; //Debe consumir el metodo de cifrado
+
+            var usuario = _contexto.TblFemaUsuarios.FirstOrDefault(u => u.Estado == true && u.UserName == credentials.Nombre && u.Clave == encryptedPassword);
+
+            if (usuario == null)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(Token.GenerarToken(credentials.Nombre));
+        }
+    }
+}
